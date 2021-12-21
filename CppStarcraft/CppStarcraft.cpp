37a36cb1d9438dbc1fd17bf3a32f68ffe67832f1
codeprobe7 <codeprobe7@gmail.com>
2021-12-22 01:15:50 +0900

    Commit

--- CppStarcraft/CppStarcraft.cpp
+++ CppStarcraft/CppStarcraft.cpp
@@ -1,6 +1,10 @@
 
+#include <iostream>
+
+#include "Library/FVector2.h"
+
 int main(int argc, char* argv[])
 {
-    
+
     return 0;
 }
---
+++ CppStarcraft/Library/FVector2.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+struct FVector2
+{
+    int X;
+    int Y;
+
+    FVector2()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+    FVector2(int _X, int _Y)
+    {
+        X = _X;
+        Y = _Y;
+    }
+};
